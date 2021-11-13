3a9d62a46bcd8c40acecb2a93e195b54ea257f34
SangHyunGil <zizon5941@gmail.com>
2021-11-13 19:55:46 +0900

    Solving Baekjoon

--- Baekjoon/baekjoon_2346(deque)py.py
+++ Baekjoon/baekjoon_2346(deque)py.py
@@ -1,19 +1,15 @@
-import sys
-input = sys.stdin.readline
+import sys 
+S1 = sys.stdin.readline().strip().upper() 
+S2 = sys.stdin.readline().strip().upper() 
+len1 = len(S1) 
+len2 = len(S2) 
+matrix = [[0] * (len2 + 1) for _ in range(len1 + 1)] 
+for i in range(1, len1 + 1): 
+    for j in range(1, len2 + 1): 
+        if S1[i - 1] == S2[j - 1]: 
+            matrix[i][j] = matrix[i - 1][j - 1] + 1 
+        else: 
+            matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1]) 
 
-for _ in range(int(input())):
-    n = int(input())
+print(matrix[-1][-1])
 
-    answer = [0]
-    k = 1
-    for i in range(n):
-        answer.append(answer[-1] | k)
-        k += 1
-
-    k = 1
-    for i in range(n-1):
-        answer.append(answer[-1] ^ k)
-        k = k << 1
-
-    print(*list(map(bin, answer)))
-    print(*answer)
