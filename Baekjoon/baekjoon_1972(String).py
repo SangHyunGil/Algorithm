15bb93bcff6ed20832a7f87cd5a13bbc98de19c1
SangHyunGil <zizon5941@gmail.com>
2021-12-04 23:00:02 +0900

    Solving Backjoon

---
+++ Baekjoon/baekjoon_1972(String).py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, sys.stdin.readline().split())
+if a == b:
+    print(0)
+else:
+    n = int((b - a) ** 0.5)
+    if n ** 2 == b - a:
+        print(2 * n - 1)
+    else:
+        z = (b - a) - n ** 2
+        print(z, n)
+        if z <= n:
+            print(2 * n)
+        else:
+            print(2 * n + 1)
