72e91eaddd2bcf10784f3d1e0b2edd6d279d319d
SangHyunGil <zizon5941@gmail.com>
2022-01-03 15:24:35 +0900

    Solving Baekjoon, Programmers

--- Baekjoon/baekjoon_16932(bfs).py
+++ Baekjoon/baekjoon_16932(bfs).py
@@ -7,39 +7,60 @@
 그래서 다른 방법으로 모든 영역의 넓이를 찾고 각 넓이를 차지하는 영역들에 번호를 매겨
 불가능한 점을 기준으로 4방향을 겹치지 않게 넓이를 더해 처리하는 방식을 생각했다.
 """
+import sys
 from collections import deque
+input = sys.stdin.readline
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-dic = {"S" : 0, "R" : 3, "L" : 1}
+def isValid(i, j):
+    if 0 <= i < N and 0 <= j < M:
+        return True
+    else:
+        return False
 
-def calculate(k, i, j, grid, visited):
-    cnt = 1
-    queue = deque([[k, i, j]])
-    visited[k][i][j] = 1
+def bfs(i, j):
+    global cnt 
+    ans = 1
+    cnt += 1
+    visited[i][j] = cnt
+    queue = deque([[i, j]])
 
     while queue:
-        d, x, y = queue.popleft()
-        nd = (d + dic[grid[x][y]]) % 4
-        nx, ny = (x+dx[nd])%len(grid), (y+dy[nd])%len(grid[0])
+        x, y = queue.popleft()
 
-        if not visited[nd][nx][ny]:
-            cnt += 1
-            visited[nd][nx][ny] = 1
-            queue.append([nd, nx, ny])
+        for k in range(4):
+            nx, ny = x+dx[k], y+dy[k]
 
-    return cnt
+            if isValid(nx, ny) and not visited[nx][ny] and graph[nx][ny]:
+                ans += 1
+                visited[nx][ny] = cnt
+                queue.append([nx, ny])
 
-def solution(grid):
-    answer = []
-    visited = [[[0] * len(grid[0]) for i in range(len(grid))] for _ in range(4)]
+    return ans
 
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            for k in range(4):                
-                if not visited[k][x][y]:
-                    answer.append(calculate(k, x, y, grid, visited))
+cnt = 0
+answer = 0
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+dic = dict()
 
-    return sorted(answer)   
+for i in range(N):
+    for j in range(M):
+        if not visited[i][j] and graph[i][j]:
+            dic[cnt] = bfs(i, j)
 
-print(solution(["R"]))
+for i in range(N):
+    for j in range(M):
+        if not visited[i][j]:
+            ans = 0
+            temp = set()
+            for k in range(4):
+                nx, ny = i+dx[k], j+dy[k]
+                if isValid(nx, ny) and visited[nx][ny] and visited[nx][ny] not in temp:
+                    ans += dic[visited[nx][ny]]
+                    temp.add(visited[nx][ny])
+            answer = max(answer, ans)
+
+print(answer+1)
